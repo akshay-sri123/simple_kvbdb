11d261ba8954ece40557003ae2e1006fbdbdbe83
akshay-sri123 <akshay@one2n.in>
2021-01-07 18:31:59 +0530

    Using kwargs for parameters

--- kvdb/kvdb.py
+++ kvdb/kvdb.py
@@ -1,14 +1,18 @@
-def kv_set(kv_dict, key, value):
+def kv_set(kv_dict, **kwargs):
+    key = kwargs.get("key")
+    value = kwargs.get("value")
     kv_dict[key] = value
     return kv_dict
 
-def kv_get(kv_dict, key):
+def kv_get(kv_dict, **kwargs):
+    key = kwargs.get("key")
     if key in kv_dict:
         return kv_dict[key]
     else:
         return None
 
-def kv_del(kv_dict, key):
+def kv_del(kv_dict, **kwargs):
+    key = kwargs.get("key")
     if key in kv_dict:
         _ = kv_dict.pop(key)
         return kv_dict
--- tests/test_kvdb.py
+++ tests/test_kvdb.py
@@ -11,38 +11,38 @@ class TestKVDB(unittest.TestCase):
         self.initial_kvdb_length = len(self.test_kvdb)
 
     def test_kv_set(self):
-        res = kv_set(self.test_kvdb, self.test_key, self.test_value)
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
 
         self.assertEqual(1, (len(res) - self.initial_kvdb_length))
         self.assertEqual(self.test_value, self.test_kvdb[self.test_key])
     
     def test_kv_set_update_existing_key(self):
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value)
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value_updated)
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
+        self.test_kvdb = kv_set(res, **{"key": self.test_key, "value": self.test_value_updated})
 
         self.assertEqual(self.test_value_updated, self.test_kvdb[self.test_key])
 
     def test_kv_get(self):
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value)
-        res = kv_get(self.test_kvdb, self.test_key)
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
+        res = kv_get(self.test_kvdb, **{"key": self.test_key})
 
         self.assertEqual(self.test_value, res)
     
     def test_kv_get_non_existent_key(self):
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value)
-        res = kv_get(self.test_kvdb, "abc")
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
+        res = kv_get(self.test_kvdb, **{"key": "abc"})
 
         self.assertEqual(None, res)
 
     def test_kv_del(self):
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value)
-        res = kv_del(self.test_kvdb, self.test_key)
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
+        res = kv_del(self.test_kvdb, **{"key": self.test_key})
 
         self.assertNotIn(self.test_key, res)
     
     def test_kv_del_non_existent_key(self):
-        self.test_kvdb = kv_set(self.test_kvdb, self.test_key, self.test_value)
-        res = kv_del(self.test_kvdb, "xyz")
+        res = kv_set(self.test_kvdb, **{"key": self.test_key, "value": self.test_value})
+        res = kv_del(self.test_kvdb, **{"key": "xyz"})
         
         self.assertEqual(None, res)
     
